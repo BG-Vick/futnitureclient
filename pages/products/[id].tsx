c48a1167a80269993ad5784ae3d7fc5e86299224
Gennadii Beriukaev <gelany19891989@gmail.com>
2023-09-24 17:53:40 +0300

    next/image updated

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -55,7 +55,7 @@ const OneProduct: NextPage<PageProps> = ({ product }) => {
                 <Image
                   className="w-full h-full   max-w-[300px] lg:max-w-md sm:max-w-[350px] object-cover"
                   src={
-                    `http://${process.env.NEXT_PUBLIC_DB_HOST}:${process.env.NEXT_PUBLIC_PORT}/` +
+                    `http://${process.env.NEXT_PUBLIC_DB_HOST}/` +
                     img
                   }
                   width={400}
